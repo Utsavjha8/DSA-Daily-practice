e5c1d54a765e671e814550a7c4e136d08b4c266c
Utsavjha8 <138434012+Utsavjha8@users.noreply.github.com>
2026-09-19 04:36:05 +0530

    Add files via upload

---
+++ binary_to_decimal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    int rem, ans = 0, mul = 1;
+    
+    while(num>0){
+        rem = num%10;
+        num = num/10;
+        ans = rem * mul + ans;
+        mul*=2;
+    }
+    
+    cout<<ans;
+
+    return 0;
+}
