2fe292143f939f83089a7cbf39a8916eb205828f
Utsavjha8 <138434012+Utsavjha8@users.noreply.github.com>
2026-09-21 01:17:23 +0530

    Add files via upload

---
+++ Power_of_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter an Integer: ";
+    cin >> n;
+
+    if(n < 1){
+        cout << 0;
+        return 0;
+    }
+
+    while(n > 1)
+    {
+        if(n % 2 == 1){
+            cout << 0;
+            return 0;
+        }
+
+        n = n / 2;
+    }
+
+    cout << 1;
+
+    return 0;
+}
